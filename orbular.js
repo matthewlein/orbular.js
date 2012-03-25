5c4450901c6c85d95379f4055390e927add5899d
matthewlein <matthewlein@hotmail.com>
2012-03-25 11:39:19 -0400

    hash options working

--- orbular.js
+++ orbular.js
@@ -19,15 +19,82 @@ function DEG_RAD(deg) {
     return (Math.PI/180) * deg;
 }
 
-function setHash() {
+function setHash(obj) {
 	if ( history.replaceState ) {
-	    history.replaceState( JSON.stringify(values),null,'#'+JSON.stringify(values) )
+	    history.replaceState( JSON.stringify(obj),null,'#'+JSON.stringify(obj) )
 	}
 }
 
 function getHash() {
-    return JSON.parse( location.hash.replace('#','') );
+    try {
+        return JSON.parse( location.hash.replace('#','') );
+    } catch(e) {}
 }
+
+// thx jQuery!
+function extend() {
+    var options, name, src, copy, copyIsArray, clone, target = arguments[0] || {},
+        i = 1,
+        length = arguments.length,
+        deep = false;
+
+    // Handle a deep copy situation
+    if (typeof target === "boolean") {
+        deep = target;
+        target = arguments[1] || {};
+        // skip the boolean and the target
+        i = 2;
+    }
+
+    // Handle case when target is a string or something (possible in deep copy)
+    if (typeof target !== "object" && !jQuery.isFunction(target)) {
+        target = {};
+    }
+
+    // extend jQuery itself if only one argument is passed
+    if (length === i) {
+        target = this;
+        --i;
+    }
+
+    for (; i < length; i++) {
+        // Only deal with non-null/undefined values
+        if ((options = arguments[i]) != null) {
+            // Extend the base object
+            for (name in options) {
+                src = target[name];
+                copy = options[name];
+
+                // Prevent never-ending loop
+                if (target === copy) {
+                    continue;
+                }
+
+                // Recurse if we're merging plain objects or arrays
+                if (deep && copy && (jQuery.isPlainObject(copy) || (copyIsArray = jQuery.isArray(copy)))) {
+                    if (copyIsArray) {
+                        copyIsArray = false;
+                        clone = src && jQuery.isArray(src) ? src : [];
+
+                    } else {
+                        clone = src && jQuery.isPlainObject(src) ? src : {};
+                    }
+
+                    // Never move original objects, clone them
+                    target[name] = jQuery.extend(deep, clone, copy);
+
+                    // Don't bring in undefined values
+                } else if (copy !== undefined) {
+                    target[name] = copy;
+                }
+            }
+        }
+    }
+
+    // Return the modified object
+    return target;
+}
+
 
 
 window.requestAnimFrame = (function(){
@@ -46,18 +113,21 @@ window.requestAnimFrame = (function(){
 // --------------------------------------------------------------------- //
 
 // holds all orbs
-var OrbPile = function() {
+var OrbPile = function(options) {
     
-    this.orbDensity = 0.5;
-    this.arms = 5;
-    this.orbColor = "#ffae23";
-    this.armLength = 200;
-    this.orbSize = 30;
-    this.angle = 0;
-    this.rotateSpeed = 0.1;
-    this.centerRadius = 30;
-    this.motionBlur = 0.02;
-    this.bgColor = [255,255,255];
+    this.opts = options;
+    
+    this.orbDensity = this.opts.orbDensity;
+    this.arms = this.opts.arms;
+    this.orbColor = this.opts.orbColor;
+    this.armLength = this.opts.armLength;
+    this.orbSize = this.opts.orbSize;
+    this.angle = this.opts.angle;
+    this.rotateSpeed = this.opts.rotateSpeed;
+    this.centerRadius = this.opts.centerRadius;
+    this.motionBlur = this.opts.motionBlur;
+    this.bgColor = this.opts.bgColor;
+
     
     this.orbs = [];
     
@@ -153,33 +223,33 @@ Orb.prototype = {
 
 
 
-var values = {
-    arms : 0,
-    armLength : 0,
-    orbSize : 0,
-    orbDensity : 0,
-    centerRadius : 0,
-    rotateSpeed : 0,
-    motionBlur : 0,
+var defaultOpts = {
+    arms : 5,
+    armLength : 200,
+    orbSize : 30,
+    orbDensity : 0.5,
+    centerRadius : 30,
+    rotateSpeed : 0.1,
+    motionBlur : 0.02,
     angle : 0,
-    orbColor : 0,
-    bgColor : 0
+    orbColor : "#ffae23",
+    bgColor : [255,255,255]
 }
 
+var userOpts = {};
 
 
 var init = function() {
     
-    if (window.hash) {
-        getHash();
+    if (location.hash) {
+        userOpts = getHash();
     }
     
-    // TODO
-    // have defaults obj
-    // merge defaults with hash values
-    // create orbPile with options
+    var opts = extend( {}, defaultOpts, userOpts);
     
-    var orbHolder = new OrbPile();
+
+    
+    var orbHolder = new OrbPile(opts);
 
 
     // --------------------------------------------------------------------- //
@@ -207,8 +277,8 @@ var init = function() {
     // TODO throttle?
     for (var i=0; i < gui.__controllers.length; i++) {
         gui.__controllers[i].onChange(function(value) {
-            values[this.property] = value;
-            setHash();
+            userOpts[this.property] = value;
+            setHash(userOpts);
         });
     }
     
@@ -218,6 +288,7 @@ var init = function() {
     }
     
     animate();
+    
 };
 
 init();
